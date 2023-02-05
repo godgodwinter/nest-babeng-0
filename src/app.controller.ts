d1413fdc89fa0b8a699af0b83b50ec83b36c0a1e
godgodwinter <kakadz.pro@gmail.com>
2023-02-05 15:10:23 +0700

    settings swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('tsc')
+  getHelloTs(): string {
+    return this.appService.getHello();
+  }
 }
